6080d52d2509a7a4fd70ac0b755d068cdf0c491a
Boris-Barboris <ismailsiege@gmail.com>
2017-04-23 01:49:52 +0300

    misc

--- dsubs_common/source/dsubs_common/objects/map.d
+++ dsubs_common/source/dsubs_common/objects/map.d
@@ -3,7 +3,8 @@ module dsubs_common.objects.map;
 import gfm.math.box;
 
 /// Game world map.
-/// At first versions of dsubs it's a simple.
+/// At first versions of dsubs it's a simple rectangle. No internal topology.
+/// Physical objects passing through edges will be pushed back.
 class Map
 {
     box2d bounds;
