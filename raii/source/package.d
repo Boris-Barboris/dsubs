086948655c7963b351ad04f528c900d54e883514
Boris-Barboris <ismailsiege@gmail.com>
2017-06-23 14:28:59 +0300

    convenient package for imports

--- raii/source/package.d
+++ raii/source/package.d
@@ -1,5 +1,32 @@
 module raii;
 
-public import raii.unique;
+import raii.unique;
+import raii.refcounted;
+import raii.closure;
+import raii.utils;
+
+template AllocationContext(Allocator = Mallocator, bool Atomic = true)
+    if (isAllocator!Allocator)
+{
+    template Unique(T)
+    {
+        alias Unique = raii.unique.Unique!(T, Allocator);
+    }
+
+    template RefCounted(T)
+    {
+        alias RefCounted = raii.refcounted.RefCounted!(T, Atomic, Allocator);
+    }
+
+    template Delegate(Ret, Args...)
+    {
+        alias Delegate = raii.closure.AllocationContext!(Allocator, Atomic).Delegate!(Ret, Args);
+    }
+
+    template autodlg(ExArgs...)
+    {
+        alias autodlg = raii.closure.AllocationContext!(Allocator, Atomic).autodlg!(ExArgs);
+    }
+}
 
 void main(){}
