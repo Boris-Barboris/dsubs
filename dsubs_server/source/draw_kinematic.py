dc2be3f64f59210458ae3afebaa21493df000554
Boris-Barboris <ismailsiege@gmail.com>
2019-05-17 17:00:01 +0300

    working plotting

---
+++ dsubs_server/source/draw_kinematic.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import glob
+import sys
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+import ntpath
+from numpy import genfromtxt
+
+testName = sys.argv[1]
+filePattern = "test_data/" + testName + "*.csv"
+print(filePattern)
+files = glob.glob(filePattern)
+print(files)
+fileRegex = re.compile('.*_(\w+).csv')
+
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+for entityDataFile in files:
+    baseFileName = ntpath.basename(entityDataFile)
+    match = fileRegex.match(baseFileName)
+    if match:
+        data = genfromtxt(entityDataFile, delimiter=',', skip_header=1)
+        # print(data)
+        pos_x = data[:, 1]
+        pos_y = data[:, 2]
+        dir_x = data[:, 3]
+        dir_y = data[:, 4]
+        vel_x = data[:, 5]
+        vel_y = data[:, 6]
+        groups = match.groups()
+        entityName = groups[0]
+        q = ax.quiver(pos_x, pos_y, dir_x, dir_y, scale_units='xy', scale=1, width=0.001)
+        ax.quiverkey(q, X=0.05, Y=1.05, U=10, label=entityName + " direction")
+        q = ax.quiver(pos_x, pos_y, vel_x, vel_y, scale_units='xy', scale=1,
+            width=0.002, color='r')
+        ax.quiverkey(q, X=0.25, Y=1.05, U=10, label=entityName + " velocity")
+
+plt.show()
--- dsubs_server/source/dsubs_server/tests/steering.d
+++ dsubs_server/source/dsubs_server/tests/steering.d
@@ -1,25 +1,52 @@
 module dsubs_server.tests.steering;
 
+import std.stdio;
+import std.file: mkdirRecurse;
+
 import dsubs_common.api.protocols.backend;
 import dsubs_common.math;
 
 import dsubs_server.common;
 import dsubs_server.entitydb;
 import dsubs_server.submarine;
+import dsubs_server.dynamics: RigidBody;
 
+
+File writeRbodyCsvHeader(string testName, string entityName)
+{
+	mkdirRecurse("test_data/steering/");
+	File f = File("test_data/steering/" ~ testName ~ "_" ~ entityName ~ ".csv", "w");
+	f.writeln("world_time,pos_x,pos_y,dir_x,dir_y,vel_x,vel_y");
+	return f;
+}
+
+void writeRbodyCsvRow(File file, usecs_t worldTime, RigidBody rb)
+{
+	vec2d rotVec = rb.kinet.forward * rb.kinet.velLength;
+	file.writefln!"%d,%f,%f,%f,%f,%f,%f"(
+		worldTime, rb.kinet.pos.x, rb.kinet.pos.y, rotVec.x, rotVec.y,
+		rb.kinet.vel.x, rb.kinet.vel.y);
+}
 
 unittest
 {
-	Globals.buildForTests();
+	File csvFile = writeRbodyCsvHeader("stork_turn", "stork");
 	SpawnReq req = SpawnReq("Stork", "Five-blade screw");
+	Globals.buildForTests();
 	Submarine s = Globals.entityDb.buildSubFromLoadout(req, null);
+	s.rigidBody.kinet.vel = vec2d(0, 16);
 	s.targetThrottle = 1.0f;
 	s.targetCourse = dgr2rad(-90);
 	s.register();
 	info("sub started at position ", s.transform.position);
-	Globals.sim.worldTimeLimit = 45 * cast(ulong)1e6;
+	Globals.sim.worldTimeLimit = 40 * cast(ulong)1e6;
+	Globals.sim.onSimulationPassStart += (usecs_t worldTime)
+	{
+		writeRbodyCsvRow(csvFile, worldTime, s.rigidBody);
+	};
 	Globals.sim.start();
 	Globals.sim.join();
 	info("sub finished at position ", s.transform.position);
+	csvFile.close();
 	Globals.resetForTests();
 }
