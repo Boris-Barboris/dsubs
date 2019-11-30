575bf741b9c73dd0502d4277a1daa89fae1774f7
Boris-Barboris <ismailsiege@gmail.com>
2019-11-30 14:58:21 +0300

    arch_libs -> dsubs_libs

--- dsubs_client/run.sh
+++ dsubs_client/run.sh
@@ -2,6 +2,6 @@
 
 set -eu
 dub build -b debug "$@"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${0%/*}/arch_libs/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${0%/*}/dsubs_libs/"
 #export ALSOFT_CONF="./alsoft.ini"
 ./dsubs_client
