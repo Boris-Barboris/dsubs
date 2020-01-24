3c32265ea5549346525398a3f4bf34582a3666c4
Boris-Barboris <ismailsiege@gmail.com>
2020-01-24 18:19:29 +0300

    build script for Iskander3 machine

---
+++ build/build_all.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e
+
+COMMIT=$(git describe)
+echo "COMMIT=${COMMIT}"
+
+echo "building Trusty"
+echo "starting lxd"
+sudo systemctl start lxd
+sleep 5
+lxc start dsubstrusty
+sleep 5
+ssh dsubstrusty "
+  set -e
+  . dlang/dmd-2.090.0/activate
+  cd dsubs/dsubs_client
+  git fetch
+  git checkout ${COMMIT}
+  dub upgrade
+  dub build -b plain -c prod
+  cd ~
+  bash bundle.sh
+"
+rsync -v dsubstrusty:~/dsubs-linux-trusty-amd64.tar.gz ./dsubs-${COMMIT}-linux-trusty-amd64.tar.gz
+echo "Trusty build OK"
+lxc stop dsubstrusty
+sudo systemctl stop lxd
+
+
+echo "building Xenial"
+echo "starting VM"
+sudo virsh start ubuntu16.04
+sleep 20
+ssh dsubsxenial "
+  set -e
+  . dlang/dmd-2.090.0/activate
+  cd dsubs/dsubs_client
+  git fetch
+  git checkout ${COMMIT}
+  dub upgrade
+  dub build -b plain -c prod
+  cd ~
+  bash bundle.sh
+"
+rsync -v dsubsxenial:~/dsubs-linux-xenial-amd64.tar.gz ./dsubs-${COMMIT}-linux-xenial-amd64.tar.gz
+echo "Xenial build OK"
+sudo virsh destroy ubuntu16.04
+
+
+echo "building Bionic"
+echo "starting VM"
+sudo virsh start ubuntu18.04
+sleep 20
+ssh dsubsbionic "
+  set -e
+  . dlang/dmd-2.090.0/activate
+  cd dsubs/dsubs_client
+  git fetch
+  git checkout ${COMMIT}
+  dub upgrade
+  dub build -b plain -c prod
+  cd ~
+  bash bundle.sh
+"
+rsync -v dsubsbionic:~/dsubs-linux-bionic-amd64.tar.gz ./dsubs-${COMMIT}-linux-bionic-amd64.tar.gz
+echo "Bionic build OK"
+sudo virsh shutdown ubuntu18.04
+
