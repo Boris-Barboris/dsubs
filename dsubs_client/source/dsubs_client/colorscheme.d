ed24aa511ca931916d0427395aff749d9d2a009d
Boris-Barboris <ismailsiege@gmail.com>
2020-05-30 13:54:44 +0300

    make grid lines a bit brighter

--- dsubs_client/source/dsubs_client/colorscheme.d
+++ dsubs_client/source/dsubs_client/colorscheme.d
@@ -39,7 +39,7 @@ struct ColorScheme
 	sfColor simMessageFont = sfColor(189, 135, 135, 255);
 	sfColor loadoutHint = sfColor(150, 150, 150, 255);
 	sfColor tubeCircle = sfColor(255, 94, 0, 255);
-	sfColor mapGrid = sfColor(130, 112, 112, 20);
+	sfColor mapGrid = sfColor(130, 112, 112, 23);
 }
 
 /// Global color scheme
