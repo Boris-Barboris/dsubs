662a3b4e10cc6d2c9be20aa2df15b8bb7e1e50ce
Boris-Barboris <ismailsiege@gmail.com>
2020-10-25 23:47:12 +0300

    windows x64 build attempt

--- build/build_all.sh
+++ build/build_all.sh
@@ -8,14 +8,14 @@ echo "building Windows"
 echo "starting VM"
 sudo virsh start win7
 sleep 60
-ssh win7 "cd dsubs\\dsubs_client && git fetch --force && git fetch --tags --force && git checkout ${COMMIT} && git submodule update --init && dub upgrade && dub build --arch=x86 -c prod -b plain"
+ssh win7 "cd dsubs\\dsubs_client && git fetch --force && git fetch --tags --force && git checkout ${COMMIT} && git submodule update --init && dub upgrade && dub build --arch=x86_64 -c prod -b plain"
 scp win7:dsubs/dsubs_client/dsubs_client.exe ./dsubs_client.exe
 mkdir dsubs
 mv dsubs_client.exe dsubs/
 rsync -ar ../dsubs_client/fonts dsubs/
-rsync -ar ../dsubs_client/libs-win-x86/*.dll dsubs/
+rsync -ar ../dsubs_client/libs-win-x64/*.dll dsubs/
 cp ../dsubs_client/alsoft.ini dsubs/
-zip -r dsubs-${COMMIT}-win-x86.zip dsubs
+zip -r dsubs-${COMMIT}-win-x64.zip dsubs
 rm -rf dsubs
 echo "Windows build OK"
 sudo virsh shutdown win7
