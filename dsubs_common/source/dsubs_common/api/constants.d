0c65f0e66d41d0411585592f3e02c94ee234d7c0
Boris-Barboris <ismailsiege@gmail.com>
2021-03-28 20:30:06 +0300

    delete inlined dsubs_common

--- dsubs_common/source/dsubs_common/api/constants.d
+++
@@ -1,7 +0,0 @@
-module dsubs_common.api.constants;
-
-/// Safety limit on message size (bytes)
-enum int MAX_MSG_SIZE = 100000000;	// 100 Mb
-
-/// Time type, microseconds (1e-6 seconds)
-alias usecs_t = long;
--- dsubs_common/source/dsubs_common/api/package.d
+++
@@ -1,7 +0,0 @@
-module dsubs_common.api;
-
-public import dsubs_common.api.constants;
-public import dsubs_common.api.entities;
-public import dsubs_common.api.encryption;
-public import dsubs_common.api.protocol;
-public import dsubs_common.api.utils;
