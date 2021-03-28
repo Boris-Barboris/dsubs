1ac8037740f75a1f1ab3357480b47d48966f529c
Boris-Barboris <ismailsiege@gmail.com>
2021-03-28 21:00:08 +0300

    delete dsubs_client

--- dsubs_client/source/dsubs_client/game/cic/protocol.d
+++
@@ -1,7 +0,0 @@
-module dsubs_client.game.cic.protocol;
-
-import dsubs_common.api.protocol;
-
-import dsubs_client.game.cic.messages;
-
-alias CICProtocol = Protocol!"dsubs_client.game.cic.messages";
